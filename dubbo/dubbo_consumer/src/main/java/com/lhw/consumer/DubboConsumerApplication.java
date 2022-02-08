1e8b8cfb25463fc0fff9ef672d501d621394458f
linhw <953522392@qq.com>
2022-02-08 14:35:09 +0800

    fix(dubbo):dubbo高级应用——服务分组

--- dubbo/dubbo_consumer/src/main/java/com/lhw/consumer/DubboConsumerApplication.java
+++ dubbo/dubbo_consumer/src/main/java/com/lhw/consumer/DubboConsumerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath:config/consumer-moreRegistry.xml"})
+@ImportResource({"classpath:config/consumer-group.xml"})
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
