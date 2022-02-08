87dd42baf88396f4b7487a3cba27f8b686c353a5
linhw <953522392@qq.com>
2022-02-08 14:52:01 +0800

    fix(dubbo):dubbo高级应用——多版本控制

--- dubbo/dubbo_provider/src/main/java/com/lhw/provider/DubboProviderApplication.java
+++ dubbo/dubbo_provider/src/main/java/com/lhw/provider/DubboProviderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath:config/provider-group.xml"})
+@ImportResource({"classpath:config/provider-version.xml"})
 public class DubboProviderApplication {
 
     public static void main(String[] args) {
