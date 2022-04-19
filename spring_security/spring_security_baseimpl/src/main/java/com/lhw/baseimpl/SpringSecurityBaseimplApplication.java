eccaa6bb0ed463b1ceb0b0f938953ec8031f9efc
linhw <953522392@qq.com>
2022-04-19 10:52:54 +0800

    feat(spring_security):添加spring security安全验证模块

---
+++ spring_security/spring_security_baseimpl/src/main/java/com/lhw/baseimpl/SpringSecurityBaseimplApplication.java
@@ -0,0 +1,13 @@
+package com.lhw.baseimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBaseimplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBaseimplApplication.class, args);
+	}
+
+}
---
+++ spring_security/spring_security_baseimpl/src/test/java/com/lhw/baseimpl/SpringSecurityBaseimplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhw.baseimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBaseimplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
